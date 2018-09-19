2caf5edacb6d3ca395fdfbd85e10cbcde04eba54
disen <610039018@qq.com>
2018-09-19 14:51:29 +0800

    login_user_v0.1

---
+++ user.py
@@ -0,0 +1,13 @@
+class User():
+    def __init__(self, id, name, passwd, token):
+        self.id = id
+        self.name = name
+    
+    def save(self):
+        print('-保存用户--')
+
+
+    def login(self):
+        pass
+
+
